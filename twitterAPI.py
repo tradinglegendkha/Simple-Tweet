d08b4088836943f398aca47377f0de05f2ac6841
Buppi Khan <khanbuppi@gmail.com>
2022-04-21 18:05:22 -0400

    fixed the rate limit issue

--- twitterAPI.py
+++ twitterAPI.py
@@ -6,18 +6,19 @@ auth = tweepy.OAuthHandler(con.consumer_key, con.consumer_secret)
 auth.set_access_token(con.access_token, con.access_token_secret)
 api = tweepy.API(auth)
 
-number_of_tweets = 10
-tweets = []
-likes = []
+number_of_tweets = 10 #numbmer of tweets  
 time = []
+tweets = []
+#likes = []
 
-for i in tweepy.Cursor(api.user_timeline, id="DeItaone", tweet_mode="extended").items(number_of_tweets):
-    tweets.append(i.full_text)
-    likes.append(i.favorite_count)
-    time.append(i.created_at)
+def tweets(): 
+    for i in tweepy.Cursor(api.user_timeline, id="DeItaone", tweet_mode="extended").items(number_of_tweets):
+        try: #twitter has rate limit, so theres no way to fix the incomplete tweets
+            tweets.append(i.retweeted_status.full_text)
+        except AttributeError:
+            print(i.full_text)
+        #likes.append(i.favorite_count)
+        #time.append(i.created_at)
 
-df = pd.DataFrame({"tweets":tweets, "likes":likes, "time":time})
-
-print(df)
-
-
+#df = pd.DataFrame({"time":time, "tweets":tweets}) #data frame, how the tweets are displayed
+#print(df)
